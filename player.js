ace5b87f7ed888267d2cff31e071e56004c50e86
Myasan_DJ <yuta.the.tv1223@gmail.com>
2026-07-06 22:56:14 +0900

    Update player.js

--- player.js
+++ player.js
@@ -27,7 +27,8 @@ const playlist=[
   {src:"track14.mp3", title:"Howling", cover:"cover4.jpg"},
   {src:"track15.mp3", title:"Howling by Hoshino", cover:"Hoshino.PNG"},
   {src:"track16.mp3", title:"光の軌跡", cover:"yoakenouta.jpg"},
-  {src:"track17.mp3", title:"Suspect", cover:"cover9.jpeg"}
+  {src:"track17.mp3", title:"Suspect", cover:"cover9.jpeg"},
+  {src:"track18.mp3", title:"かぷかぷ♡フィーバーナイト", cover:"cover18.png"}
 ];
 
 const MIN_VOLUME=0.1;
